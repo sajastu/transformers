fa2bffd1f09bc6bd3f8ad4fb63ce03ebb76bb59a
sotudehg <sotudehg>
2021-08-20 18:35:27 -0400

    init

--- set_path.sh
+++ set_path.sh
@@ -11,7 +11,7 @@ done
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MODEL=bart
 export M_ID=reddit-large-tldrQ
-export DS_BASE_DIR=/home/code-base/user_space/trainman-k8s-storage-c91414be-d3d1-431d-a2c8-1d040368c6e8/tldrQ/
+export DS_BASE_DIR=$SAVE_MODEL_DIR/tldrQ/
 
 
 echo "SAVE_MODEL_DIR is $SAVE_MODEL_DIR"
